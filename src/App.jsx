f20e5d7344778c9c6b707dfc8ea77fbd6d3d6026
David Macias <henriquemaciasdavid@fpllefia.com>
2025-04-25 15:44:36 +0200

    Exercici 1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+      <header>
+        <h1>David Blanco</h1>
+      </header>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+    </>
+  )
+}
+
+export default App
